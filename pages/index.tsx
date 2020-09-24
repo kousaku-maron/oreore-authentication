47c3e7d5adfb6dd3711eee92618d36096eed3143
Kosaku Kurino <kousaku.maron@gmail.com>
2020-09-25 00:15:31 +0900

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Next App Template</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
